552fa174517342f50912f29b03dc6ef82ff15d82
evertonra <everton-adams@hotmail.com>
2024-10-25 07:09:30 -0300

    first commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Indica que o Tailwind deve procurar em todos os arquivos .html na raiz do projeto
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
