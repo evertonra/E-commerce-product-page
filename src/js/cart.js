506c0fbbe9b30a9d39060df6b8917b0f085279ff
evertonra <everton-adams@hotmail.com>
2024-10-26 11:40:14 -0300

    add: funcionalidades nas imagens e inicio do fluxo de carrinho

--- src/js/cart.js
+++ src/js/cart.js
@@ -13,3 +13,33 @@ function subQuantity() {
     document.getElementById("quantity").innerText = quantity;
   }
 }
+
+function addCart() {
+  var product = document.getElementById("product").innerText;
+  var quantity = parseInt(document.getElementById("quantity").innerText);
+  var price = parseInt(document.getElementById("price").innerText);
+  var total = quantity * price;
+
+  var cart = { product, quantity, price, total };
+  console.log("total", cart);
+  localStorage.setItem("cartList", JSON.stringify(cart));
+}
+
+function removeCart() {
+  localStorage.removeItem("cartList");
+}
+
+function formatPrice(price) {
+  return price.toLocaleString("en-US", { style: "currency", currency: "USD" });
+}
+
+function openCartModal() {
+  const modal = document.getElementById("cartModal");
+
+  if (modal.style.display === "flex") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "flex";
+  }
+  console.log("modal", modal);
+}
--- src/js/imageController.js
+++ src/js/imageController.js
@@ -7,32 +7,72 @@ const images = [
 ];
 let currentImageIndex = 0;
 
-// Função para abrir o modal
+// Função para alterar a imagem principal ao clicar na miniatura
+function changeImage(index) {
+  currentImageIndex = index;
+  const mainImage = document.querySelector(".main-product-image");
+  if (mainImage) {
+    // Verifica se o elemento existe
+    mainImage.src = images[currentImageIndex];
+    updateThumbnailClasses();
+  } else {
+    console.error("Elemento .main-product-image não encontrado.");
+  }
+}
+
 function openImageModal(index = 0) {
   currentImageIndex = index;
   const modal = document.getElementById("imageModal");
   const modalImage = document.getElementById("modalImage");
   modalImage.src = images[currentImageIndex];
   modal.style.display = "flex";
+  updateThumbnailClasses();
 }
 
-// Função para fechar o modal
 function closeModal() {
   const modal = document.getElementById("imageModal");
   modal.style.display = "none";
 }
 
-// Função para avançar para a próxima imagem
 function nextImage() {
   currentImageIndex = (currentImageIndex + 1) % images.length;
   document.getElementById("modalImage").src = images[currentImageIndex];
+  updateThumbnailClasses();
 }
 
-// Função para voltar para a imagem anterior
 function prevImage() {
   currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
   document.getElementById("modalImage").src = images[currentImageIndex];
+  updateThumbnailClasses();
 }
+
+function updateThumbnailClasses() {
+  const thumbnails = document.querySelectorAll("#imageModal .flex > div");
+  thumbnails.forEach((thumbnailContainer, index) => {
+    const thumbnailImage = thumbnailContainer.querySelector("img");
+    if (index === currentImageIndex) {
+      thumbnailContainer.classList.add("image_border");
+      thumbnailImage.classList.add("image_selected");
+    } else {
+      thumbnailContainer.classList.remove("image_border");
+      thumbnailImage.classList.remove("image_selected");
+    }
+  });
+}
+
+// function updateThumbnailClasses() {
+//   const thumbnails = document.querySelectorAll("#imageModal .flex > div");
+//   thumbnails.forEach((thumbnailContainer, index) => {
+//     const thumbnailImage = thumbnailContainer.querySelector("img");
+//     if (index === currentImageIndex) {
+//       thumbnailContainer.classList.add("image_border");
+//       thumbnailImage.classList.add("image_selected");
+//     } else {
+//       thumbnailContainer.classList.remove("image_border");
+//       thumbnailImage.classList.remove("image_selected");
+//     }
+//   });
+// }
 
 // Fecha o modal ao clicar fora do conteúdo
 window.onclick = function (event) {
